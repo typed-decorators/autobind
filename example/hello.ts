4b5f139417db12a2d923d60369ab3ca661b661e2
FUJI Goro (gfx) <gfuji@cpan.org>
2018-04-09 10:23:03 +0900

    initial commit

---
+++ example/hello.ts
@@ -0,0 +1,25 @@
+import { autobind } from "../src/autobind";
+
+
+class C {
+  readonly name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  //@autobind
+  f() {
+    console.log('this:', this);
+  }
+}
+
+const c = new C("foo");
+
+console.log('Call f() as method:');
+c.f();
+
+console.log('Call f() as function:');
+const f = c.f;
+f();
+
---
+++ src/autobind.ts
@@ -0,0 +1,22 @@
+
+export function autobind<F extends Function>(target: any, name: string, descriptor: TypedPropertyDescriptor<F>): TypedPropertyDescriptor<F> {
+  const { enumerable, configurable, value } = descriptor;
+
+  return {
+    enumerable,
+    configurable,
+
+    get() {
+      return value!.bind(this);
+    },
+
+    set(value: F) {
+      Object.defineProperty(this, name, {
+        writable: true,
+        enumerable: true,
+        configurable: true,
+        value,
+      });
+    },
+  };
+}
