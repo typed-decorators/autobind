61bce49b71b87aa2718941c3a8b39b9942f8b409
FUJI Goro (gfx) <gfuji@cpan.org>
2018-04-09 10:31:03 +0900

    add tests

---
+++ test/autobind.test.ts
@@ -0,0 +1,15 @@
+import { autobind } from "../src/autobind";
+
+class C {
+  @autobind
+  f() {
+    return this;
+  }
+}
+
+console.log('1..1'); // test anything protocol
+
+const c = new C();
+const f = c.f;
+console.assert(f() === c);
+console.log('ok 1')
